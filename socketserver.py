85fbd76f18aada1f91a26ae04795c49427e3dfa6
Kartik Madan <kartikmadan97@gmail.com>
2019-07-04 15:25:36 +0530

    added train/test func

--- socketserver.py
+++ socketserver.py
@@ -1,19 +1,36 @@
 import socket
-import numpy as np
-from sklearn.linear_model import LinearRegression
+import ast
+import build
 
-def calcregr(msg = ''):
-    chartdata = np.fromstring(msg, dtype=float, sep= ' ') 
-    Y = np.array(chartdata).reshape(-1,1)
-    X = np.array(np.arange(len(chartdata))).reshape(-1,1)
-        
-    lr = LinearRegression()
-    lr.fit(X, Y)
-    Y_pred = lr.predict(X)
-    type(Y_pred)
-    P = Y_pred.astype(str).item(-1) + ' ' + Y_pred.astype(str).item(0)
-    print(P)
-    return str(P)
+def train_test_model(msg = ''):
+    msg = msg.replace('true','True')
+    msg = ast.literal_eval(msg)
+    
+    print(type(msg))
+    
+    if(type(msg) == dict):
+        input_data = msg
+    else:
+        return "BAD JSON!!"
+    
+    data = input_data['Data']
+    date = input_data['Time']
+    file_name = input_data['FileName']
+    
+    testSize = int(input_data['TestingPart'] / 100 * len(data))
+    trainSize = len(data) - testSize
+    
+    train = build.train(training_set=data[:trainSize], date=date[:trainSize], lr=input_data['LearningRate'], scale=input_data['Scale'], epochs=input_data['Epochs'], momentum=input_data['Momentum'], optimizer=input_data['Optimizer'], file_name=file_name, architecture=input_data['Architecture'])
+    test = build.test(testing_set=data[trainSize:], date=date[trainSize:], file_name=input_data['FileName'])
+
+    print(train)
+    print(test)
+    
+    evaluate = build.evaluate(file_name=file_name, testing_weight=input_data['TestingWeight'])
+
+    print(evaluate)
+    
+    return str(evaluate)
 
 class socketserver:
     def __init__(self, address = '', port = 9090):
@@ -30,16 +47,16 @@ class socketserver:
         self.cummdata = ''
 
         while True:
-            data = self.conn.recv(10000)
+            data = self.conn.recv(10000000)
             self.cummdata+=data.decode("utf-8")
             if not data:
                 break    
-            self.conn.send(bytes(calcregr(self.cummdata), "utf-8"))
+            self.conn.send(bytes(train_test_model(self.cummdata), "utf-8"))
             return self.cummdata
             
     def __del__(self):
         self.sock.close()
-
+        
 serv = socketserver('127.0.0.1', 9090)
 
 while True:  
