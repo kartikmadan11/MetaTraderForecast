ed8b7ae6cfea2f25dfdddcfe7985221d97ae3bca
jyotimalik3 <49374294+jyotimalik3@users.noreply.github.com>
2019-07-27 11:08:23 +0530

    Removed Extra Using Declarations

--- RNNProject_NT8_Indi.cs
+++ RNNProject_NT8_Indi.cs
@@ -1,23 +1,9 @@
 #region Using declarations
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Xml.Serialization;
-using NinjaTrader.Cbi;
-using NinjaTrader.Gui;
-using NinjaTrader.Gui.Chart;
-using NinjaTrader.Gui.SuperDom;
-using NinjaTrader.Gui.Tools;
-using NinjaTrader.Data;
-using NinjaTrader.NinjaScript;
-using NinjaTrader.Core.FloatingPoint;
 using NinjaTrader.NinjaScript.DrawingTools;
 using System.Net.Sockets;
 using Newtonsoft.Json;
